51d3b05bda6a2fa4c951f5babe943402e6c48615
ChrisDemahy <demahyc@gmail.com>
2021-03-05 16:24:11 -0600

    inital backend stuff done

--- backend/app/channels/chat_channel.rb
+++ backend/app/channels/chat_channel.rb
@@ -1,12 +1,21 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    # corresponds to redis channel
+    stream_from 'chat_channel'
+
+    # Get all messages to send to client on initial connect
+    messages = Message.all
+
+    # Broadcast those messages
+    ActionCable.server.broadcast(
+      'chat_channel',
+      type: 'all_messages', message_history: messages
+    )
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak
-  end
+  def speak; end
 end
