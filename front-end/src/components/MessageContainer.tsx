6224b6f99fcea6353a18f30ff14d40942da74ac7
ChrisDemahy <demahyc@gmail.com>
2021-03-05 15:47:43 -0600

    Implemented bulma

---
+++ front-end/src/components/MessageContainer.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+
+const MessageContainer = () => {
+  return (
+    <div className="container column is-10">
+      <div className="section">
+        <div className="card">
+          <div className="card-header">
+            <p className="card-header-title">Header</p>
+          </div>
+          <div className="card-content">
+            <div className="content">Content</div>
+          </div>
+        </div>
+        <br />
+
+        <div className="card is-hidden1">
+          <div className="card-header">
+            <p className="card-header-title">Header</p>
+          </div>
+          <div className="card-content">
+            <div className="content">Content</div>
+          </div>
+        </div>
+        <br />
+
+        <div className="card is-hidden1">
+          <div className="card-header">
+            <p className="card-header-title">Header</p>
+          </div>
+          <div className="card-content">
+            <div className="content">Content</div>
+          </div>
+        </div>
+        <br />
+
+        <div className="card is-hidden1">
+          <div className="card-header">
+            <p className="card-header-title">Header</p>
+          </div>
+          <div className="card-content">
+            <div className="content">Content</div>
+          </div>
+        </div>
+        <br />
+
+        <div className="card is-hidden1">
+          <div className="card-header">
+            <p className="card-header-title">Header</p>
+          </div>
+          <div className="card-content">
+            <div className="content">Content</div>
+          </div>
+        </div>
+        <br />
+
+        <div className="card is-hidden1">
+          <div className="card-header">
+            <p className="card-header-title">Header</p>
+          </div>
+          <div className="card-content">
+            <div className="content">Content</div>
+          </div>
+        </div>
+        <br />
+      </div>
+    </div>
+  );
+  // return (
+  //   <div className="columns is-centered">
+  //     <div className="column has-text-centered is-2">
+  //       <a className="button">Button 1</a>
+  //     </div>
+  //     <div className="column has-text-centered is-2">
+  //       <a className="button">Button 2</a>
+  //     </div>
+  //     <div className="column has-text-centered is-2">
+  //       <a className="button">Button 3</a>
+  //     </div>
+  //     <div className="column has-text-centered is-2">
+  //       <a className="button">Button 4</a>
+  //     </div>
+  //   </div>
+  // );
+};
+
+export default MessageContainer;
